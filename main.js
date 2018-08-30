5a63e97ba0672e13b54f420559aebf9d2bd4eccb
Israel Garcia <igarcia@itexico.net>
2018-08-30 16:50:53 -0500

    documentation + return values added

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
+/**
+ *  This method evaluates two given strings and returns a boolean
+ *  value if both are an Anagram. 
+ *  For more information about anagrams:
+ * @see https://en.wikipedia.org/wiki/Anagram
+ * @param {String} str1 
+ * @param {String} str2 
+ * @returns {Boolean}
+ * @author SarahWinchester
+ */
 function anagram(str1, str2) {
-    
-    if(str1.length !== str2.length){
-        console.log(str1 + " and "+ str2+ " they are not anagrams");
-        return "This is not an anagram"  
-    }else{
+    if(str1.length == str2.length){
         let strsplit1= str1.toLowerCase().split("").sort().join();
-        console.log(strsplit1);
         let strsplit2= str2.toLowerCase().split("").sort().join();
-        console.log(strsplit2)
-        if (strsplit1 === strsplit2) {
-            console.log( str1+ " and " + str2 + " are anagrams");
-            return str1+ " and " + str2 + " are anagrams"
-        }else{
-            console.log(str1 + " and "+ str2+ " they are not anagrams");
-        }
+        if (strsplit1 === strsplit2) 
+            return true;    
+        else
+            return false;
     }
+    else 
+        return false;
 }
-anagram("Lima", "Roma");
